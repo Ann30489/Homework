0d80f362f17fe929e82db508c9b3453e5cdd2edb
Ann30489 <annette304kr@gmail.com>
2021-09-20 16:37:50 +0300

    adaptive navigation

---
+++ cahee-sidebar-menu/_js/_script.js
@@ -0,0 +1,10 @@
+function openNav() {
+	document.getElementsByClassName("c-navbar")[0].style.width = "250px";
+	document.getElementsByClassName("c-main--collapse")[0].style.marginLeft = "250px";
+
+}
+
+function closeNav() {
+	document.getElementsByClassName("c-navbar")[0].style.width = "0";
+	document.getElementsByClassName("c-main--collapse")[0].style.marginLeft = "0";
+}
