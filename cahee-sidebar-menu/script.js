b0f30a58284767e204002d3977960a21baf9f167
Ann30489 <annette304kr@gmail.com>
2021-11-23 19:55:25 +0200

    bootstrap homework

--- cahee-sidebar-menu/script.js
+++ cahee-sidebar-menu/script.js
@@ -1,10 +1,14 @@
 function openNav() {
 	document.getElementsByClassName("c-navbar")[0].style.width = "250px";
 	document.getElementsByClassName("c-main--collapse")[0].style.marginLeft = "250px";
+	document.body.classList.add("lock");
+	// document.body.style.overflow = "hidden";
 
 }
 
 function closeNav() {
 	document.getElementsByClassName("c-navbar")[0].style.width = "0";
 	document.getElementsByClassName("c-main--collapse")[0].style.marginLeft = "0";
+	document.body.classList.remove("lock");
+	// document.body.style.overflow = "visible";
 }
